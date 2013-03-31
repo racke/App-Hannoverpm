1cdbf87e3a4f987a24327eddfceb6943756e24aa
BURNERSK <burnersk@cpan.org>
2013-03-31 20:29:37 +0200

    Apply no-sidebar layout and index page.
    NO DYNAMIC CONTENT!!!

---
+++ public/scripts/app-hannoverpm.js
@@ -0,0 +1,3 @@
+/*! App::Hannoverpm - Homepage of Perl Mongers group in Hanover, Germany.
+ * @copyright © 2013, Hannover.pm.
+ * @license http://creativecommons.org/publicdomain/zero/1.0/ CC0 1.0 Universal */
