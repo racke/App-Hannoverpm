42bb0df7d65907a65d3e5ab7177e854c60b3668b
BURNERSK <burnersk@cpan.org>
2013-04-15 12:18:48 +0200

    smoother animation

--- public/scripts/acme.js
+++ public/scripts/acme.js
@@ -7,6 +7,7 @@ $('#header-logo-title')
 $('#header-logo-slogan')
   .css('position', 'absolute')
   .css('margin-top', '12.5em');
+var acme_fressen_ani2 = false;
 $('#header-logo > a').append(
   $('#header-logo-title')
     .clone()
@@ -16,14 +17,25 @@ $('#header-logo > a').append(
     .css('top', '0.88em')
     .css('margin-left', '15em')
     .animate({
-      'margin-left': '7em'
-    }, 2000, function(){
-      $('#header-logo-title')
-        .animate({
-          'margin-left': '-100em'
-        }, 4000);
-      $('#header-logo-title-2').animate({
         'margin-left': '20pt'
-      }, 1000);
-    })
+      },{
+        duration: 2000,
+        step: function (now, fx) {
+          if (now < 240 && acme_fressen_ani2 == false) {
+            acme_fressen_ani2 = true;
+            $('#header-logo-title')
+              .animate({
+                  'margin-left': '-20em'
+                },{
+                  duration: 1000,
+                  complete: function(){
+                    $('#header-logo-title').remove();
+                    $('#header-logo-title-2').attr('id', 'header-logo-title');
+                  }
+                }
+              );
+          }
+        }
+      }
+    )
 );
