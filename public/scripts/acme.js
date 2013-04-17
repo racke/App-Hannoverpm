3be190c7d04fa36e911fd3a2607e0a0cbc54f8b3
BURNERSK <burnersk@cpan.org>
2013-04-17 12:02:36 +0200

    Acme::Fressen activates itselfs when string "SONDERESSEN" found. Acme::Fressen has now fixed positions (don't vary anymore on different screen sizes)

--- public/scripts/acme.js
+++ public/scripts/acme.js
@@ -2,40 +2,48 @@
  * @copyright © 2013, Hannover.pm.
  * @license http://creativecommons.org/publicdomain/zero/1.0/ CC0 1.0 Universal */
 //$('#header-logo-title').html('Fressen.pm');
-$('#header-logo-title')
-  .css('position', 'absolute');
-$('#header-logo-slogan')
-  .css('position', 'absolute')
-  .css('margin-top', '12.5em');
-var acme_fressen_ani2 = false;
-$('#header-logo > a').append(
-  $('#header-logo-title')
-    .clone()
-    .attr('id', 'header-logo-title-2')
-    .attr('class', 'header-logo-title')
-    .html('Fressen.pm')
-    .css('top', '0.88em')
-    .css('margin-left', '15em')
-    .animate({
-        'margin-left': '20pt'
-      },{
-        duration: 2000,
-        step: function (now, fx) {
-          if (now < 240 && acme_fressen_ani2 == false) {
-            acme_fressen_ani2 = true;
-            $('#header-logo-title')
-              .animate({
-                  'margin-left': '-20em'
-                },{
-                  duration: 1000,
-                  complete: function(){
-                    $('#header-logo-title').remove();
-                    $('#header-logo-title-2').attr('id', 'header-logo-title');
-                  }
-                }
-              );
+$(document).ready(function() {
+  var essen_regexp = /SONDERESSEN/i;
+  var essen_match = essen_regexp.exec($(document).text());
+  if ( essen_match != null ) {
+    $('#header-logo-title')
+      .css('position', 'absolute');
+    $('#header-logo-slogan')
+      .css('position', 'absolute')
+      .css('margin-top', '12.5em');
+    var acme_fressen_ani2 = false;
+    $('#header-logo > a').append(
+      $('#header-logo-title')
+        .clone()
+        .css('display', 'none')
+        .attr('id', 'header-logo-title-2')
+        .attr('class', 'header-logo-title')
+        .html('Fressen.pm')
+        .css('top', '0.88em')
+        .css('margin-left', '500pt')
+        .css('display', 'block')
+        .animate({
+            'margin-left': '20pt'
+          },{
+            duration: 2000,
+            step: function (now, fx) {
+              if (now < 240 && acme_fressen_ani2 == false) {
+                acme_fressen_ani2 = true;
+                $('#header-logo-title')
+                  .animate({
+                      'margin-left': '-500pt'
+                    },{
+                      duration: 1000,
+                      complete: function(){
+                        $('#header-logo-title').remove();
+                        $('#header-logo-title-2').attr('id', 'header-logo-title');
+                      }
+                    }
+                  );
+              }
+            }
           }
-        }
-      }
-    )
-);
+        )
+    );
+  }
+});
